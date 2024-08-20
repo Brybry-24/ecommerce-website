2e37eb895aa0adf50e9d7ea6de9b0fb02523d9f1
brybry <85399273+Brybry-24@users.noreply.github.com>
2024-08-20 21:08:03 +0800

    Update scriptemail.js

--- js/scriptemail.js
+++ js/scriptemail.js
@@ -1,14 +1,10 @@
-function sendMail() {
+function senMail(){
     let params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        number: document.getElementById("number").value,
-        message: document.getElementById("message").value,
-    };
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        number : document.getElementById("number").value,
+        message : document.getElementById("message").value,
+    }
 
-    emailjs.send("service_b14je8k", "template_jqfam1c", params).then(function(response) {
-        swal("Email was sent!");
-    }).catch(function(error) {
-        swal("Oops... Something went wrong: " + error.text);
-    });
+    emailjs.send("service_b14je8k","template_jqfam1c",params).then(alert("Email Sent!!"))
 }
